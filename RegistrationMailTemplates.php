12b07bf0bd130122ca87940ea465331bdb626d4d
Jan Reuteler <jan.reuteler@terminal42.ch>
2013-01-15 15:30:25 +0100

    Adjustments  for admin-email

--- RegistrationMailTemplates.php
+++ RegistrationMailTemplates.php
@@ -10,12 +10,12 @@
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation, either
  * version 3 of the License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this program. If not, please visit the Free
  * Software Foundation website at <http://www.gnu.org/licenses/>.
@@ -70,6 +70,9 @@ class RegistrationMailTemplates extends System
 		{
 			$objEmail = new EmailTemplate($objModule->mail_template);
 			$objEmail->send($arrData['email'], $arrData);
+
+			$objAdminEmail = new EmailTemplate($objModule->admin_mail_template);
+			$objAdminEmail->send($GLOBALS['TL_ADMIN_EMAIL'], $arrData);
 		}
 		catch (Exception $e)
 		{
--- dca/tl_module.php
+++ dca/tl_module.php
@@ -10,12 +10,12 @@
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation, either
  * version 3 of the License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this program. If not, please visit the Free
  * Software Foundation website at <http://www.gnu.org/licenses/>.
@@ -32,7 +32,7 @@
 /**
  * Extend tl_module palettes
  */
-$GLOBALS['TL_DCA']['tl_module']['palettes']['registration'] = str_replace('reg_activate;', 'reg_activate,mail_template;', $GLOBALS['TL_DCA']['tl_module']['palettes']['registration']);
+$GLOBALS['TL_DCA']['tl_module']['palettes']['registration'] = str_replace('reg_activate;', 'reg_activate,mail_template,admin_mail_template;', $GLOBALS['TL_DCA']['tl_module']['palettes']['registration']);
 $GLOBALS['TL_DCA']['tl_module']['palettes']['lostPassword'] = str_replace('reg_jumpTo,', 'reg_jumpTo,mail_template,', $GLOBALS['TL_DCA']['tl_module']['palettes']['lostPassword']);
 
 ?>
